3f49927cce61a49083e48eced6561d5721efcb06
Sam Arnold <samarnold@Sams-MacBook-Pro.local>
2021-06-26 16:43:54 -0400

    Removed navbar from app component to avoid premature deployment

--- client/App.jsx
+++ client/App.jsx
@@ -1,11 +1,9 @@
 import React,{useState} from 'react'
 import LoginForm from './Components/loginForm.jsx'
-import MenuDrawer from './Components/menuDrawer.jsx';
 
 const App = () => (
     <div>
         {/* <h1>Hello React</h1> */}
-        <MenuDrawer />
         <LoginForm />
     </div>
 )
