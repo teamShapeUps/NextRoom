a9dc8375fb2def3a36650761990b029d2dac07d1
Sergey01 <Sergey01@users.noreply.github.com>
2021-06-26 17:12:31 -0400

    Created Map.jsx component using react-leaflet. I was going to mount it on map.html but stuck it in but stuck it in App.jsx instead. Currently it shows up but has hardcoded coords. Will implement geolocation grabbeolocation grabbing from browser next.

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,12 @@
-import React,{useState} from 'react'
-import LoginForm from './Components/loginForm.jsx'
+import React,{useState} from 'react';
+import LoginForm from './Components/loginForm.jsx';
+import Map from './Components/Map.jsx';
 
 const App = () => (
     <div>
         <h1>Hello React</h1>
         <LoginForm />
+        <Map />
     </div>
 )
 
