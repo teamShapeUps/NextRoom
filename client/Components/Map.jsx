5b3068027843c34c5b0e7ed13108627615abf51e
Sergey01 <Sergey01@users.noreply.github.com>
2021-06-29 12:23:20 -0400

    Updated Map component to center on user's location on map click or touch.

--- client/Components/Map.jsx
+++ client/Components/Map.jsx
@@ -1,7 +1,7 @@
 import { makeStyles } from "@material-ui/core";
 import React, {useEffect, useState} from "react";
 // import { makeStyles } from "@material-ui/core/styles";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
 import ToiletCard from './toiletCard.jsx';
 
 // toilet dependency injection goes here:
@@ -29,14 +29,33 @@ export default function Map() {
   const classes = useStyles();
 
   const [coords, setCoords] = useState([40.785091, -73.968285]);
-  
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      console.log("Latitude is :", position.coords.latitude);
-      console.log("Longitude is :", position.coords.longitude);
-      setCoords([position.coords.latitude, position.coords.longitude])
+
+  function LocationMarker() {
+    const map = useMapEvents({
+      click() {
+        map.locate()
+      },
+      locationfound(e) {
+        setCoords(e.latlng)
+        // map.flyTo(e.latlng, map.getZoom())
+        map.flyTo(e.latlng, map.getZoom())
+      },
     })
-  }, coords);
+  
+    return coords === null ? null : (
+      <Marker position={coords}>
+        <Popup>You are here</Popup>
+      </Marker>
+    )
+  };
+  
+  // useEffect(() => {
+  //   navigator.geolocation.getCurrentPosition(function(position) {
+  //     console.log("Latitude is :", position.coords.latitude);
+  //     console.log("Longitude is :", position.coords.longitude);
+  //     setCoords([position.coords.latitude, position.coords.longitude])
+  //   })
+  // }, coords);
 
 return (
 <MapContainer className={classes.map} center={coords} zoom={13} scrollWheelZoom={false}>
@@ -44,11 +63,11 @@ return (
     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   />
-  <Marker position={coords}>
+  <LocationMarker position={coords}>
     <Popup>
       {/* A pretty CSS3 popup. <br /> Easily customizable. */}
       <ToiletCard />
     </Popup>
-  </Marker>
+  </LocationMarker>
 </MapContainer>
 )}
