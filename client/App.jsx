75df55654280aa68036bab70db31dbc6c7872a10
stephaniebenitez <66795487+stephaniebenitez@users.noreply.github.com>
2021-06-29 14:52:33 -0700

    g

--- client/App.jsx
+++ client/App.jsx
@@ -1,11 +1,11 @@
-import React,{useState} from 'react'
-import LoginForm from './Components/loginForm.jsx'
+import React, { useState } from 'react';
+import LoginForm from './Components/loginForm.jsx';
 
 const App = () => (
-    <div>
-        <h1>Hello React</h1>
-        <LoginForm />
-    </div>
-)
+  <div>
+    <h1>Hello React</h1>
+    <LoginForm />
+  </div>
+);
 
 export default App;
