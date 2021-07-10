5f06a3b2c51523c8b12aec40d43be9adc7519239
Murphypie <Ochemypie@gmail.com>
2021-07-10 17:19:17 -0400

    Working

---
+++ server/routes/users.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { restart } = require("nodemon");
+const userControllerSQL = require("../controllers/userControllerSQL.js");
+const router = express.Router();
+// route.get('/', (req, res) => {
+//     res.status(200).send('This is the user router: successful!')
+// });
+// router.post('/', userControllerSQL.createUser, (req, res) => {
+//     return res.status(200).send('User created!')
+// })
+router.post("/usersignup", userControllerSQL.createUser, (req, res) => {
+  return res.status(200).send("User created!");
+});
