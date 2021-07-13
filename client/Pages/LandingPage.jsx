e19e99af1fc7e731c420d6e461096cba4deb8a78
Damian Allert <reddallert@outlook.com>
2021-07-13 19:50:17 -0400

    created - greeting and landingpage

---
+++ client/Pages/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Greeting from '../Components/greeting.jsx';
+
+export default function LandingPage() {
+  return (
+    <Greeting />
+  );
+}
+
+// DESIGN GOALS
+// 1. Inform the visitor in a succinct, catchy manner what page is all about
+//    a.  good image
+//    b.  supporting text
+// 2. Minimalist functionality to easily log in or sign up
+//    a.  well-placed, visually appealing buttons to either log in or sign up
